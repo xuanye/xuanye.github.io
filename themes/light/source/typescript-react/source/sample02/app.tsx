762d97b7efc37cefa2a577e3c8eed45f451fe0fc
xuanye wang <xuanye.wan@gmail.com>
2016-03-26 12:29:06 +0800

    添加typescript-react相关的示例代码

---
+++ themes/light/source/typescript-react/source/sample02/app.tsx
@@ -0,0 +1,8 @@
+﻿/// <reference path='../../typings/browser.d.ts'/>
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {HelloWorld} from "./helloworld";
+
+ReactDOM.render(<HelloWorld/>, document.getElementById("content"));
