410e5a485948eb3a8b4074f29921d0281b766a11
xuanye <xuanye.wan@gmail.com>
2026-03-24 17:27:28 +0800

    Site updated: 2026-03-24 17:27:28

---
+++ demos/typescript-react/source/sample04/app.tsx
@@ -0,0 +1,8 @@
+﻿/// <reference path='../../typings/browser.d.ts'/>
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Counter from "./counter";
+
+ReactDOM.render(<Counter/>, document.getElementById("content"));
---
+++ demos/typescript-react/source/sample06/counter.tsx
@@ -0,0 +1,32 @@
+﻿/// <reference path='../../typings/browser.d.ts'/>
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+class Counter extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                <p>
+                    <label>Counter: </label>
+                    <b>#{this.props.counter}</b>
+                </p>
+                <button onClick={e => this.props.incr() }>INCREMENT</button>
+                <span style={{ padding: "0 5px" }} />
+                <button onClick={e => this.props.decr() }>DECREMENT</button>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => state;
+
+const mapDispatchToProps = (dispatch) => ({
+    incr: () => {
+        dispatch({ type: 'INCR', by: 1 });
+    },
+    decr: () => {
+        dispatch({ type: 'INCR', by: -1 });
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
